fb400b70a1876cafbace67d82bfe514941a65312
merve21c <canannur15@gmail.com>
2022-12-12 02:22:42 +0300

    Update öğretmenler.dart

--- lib/öğretmenler.dart
+++ lib/öğretmenler.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dersprogrami/kullanici.dart';
 import 'package:dersprogrami/yonetici.dart';
 import 'package:flutter/material.dart';
@@ -9,6 +10,38 @@ class ogretmenler extends StatefulWidget {
 }
 
 class _ogretmenlerState extends State<ogretmenler> {
+TextEditingController t1 = TextEditingController();
+TextEditingController t2 = TextEditingController();
+TextEditingController t3 = TextEditingController();
+
+
+ oEkle(){
+   FirebaseFirestore.instance
+       .collection("ogretmenler")
+       .doc(t1.text)
+       .set({'isim':t1.text, 'soyad':t2.text, 'unvan': t3.text})
+       .whenComplete(() => print("öğretmen eklendi.")) ;
+
+}
+
+oGuncelle(){
+  FirebaseFirestore.instance
+      .collection("ogretmenler")
+      .doc(t1.text)
+      .update({'isim':t1.text, 'soyad':t2.text, 'unvan': t3.text})
+      .whenComplete(() => print("öğretmen güncellendi.")) ;
+
+}
+
+oSil(){
+  FirebaseFirestore.instance
+      .collection("ogretmenler")
+      .doc(t1.text)
+      .delete()
+      .whenComplete(() => print("yazı eklendi.")) ;
+
+}
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -35,6 +68,18 @@ class _ogretmenlerState extends State<ogretmenler> {
                       Navigator.push(context,MaterialPageRoute(builder: (context)=>yonetici()));
                     },
                   ),
+
+                  TextField(controller: t1,),
+                  TextField(controller: t2,),
+                  TextField(controller: t3,),
+                  Row(children: [
+                    ElevatedButton(onPressed: oEkle , child: Text("ekle")),
+                    ElevatedButton(onPressed: oGuncelle ,child: Text("güncelle")),
+                    ElevatedButton(onPressed: oSil, child: Text("sil")),
+
+
+                  ],),
+
                 ],
               ),
             ),
@@ -44,3 +89,6 @@ class _ogretmenlerState extends State<ogretmenler> {
     );
   }
 }
+
+
+
