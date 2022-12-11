5ba6490dc451fe7d89b99b7d15c093e2ca5b878f
merve21c <canannur15@gmail.com>
2022-12-11 13:36:51 +0300

    Create ogretmenlerK.dart

---
+++ lib/ogretmenlerK.dart
@@ -0,0 +1,46 @@
+import 'package:dersprogrami/kullanici.dart';
+import 'package:flutter/material.dart';
+
+class ogretmenlerK extends StatefulWidget {
+  const ogretmenlerK({Key? key}) : super(key: key);
+
+  @override
+  State<ogretmenlerK> createState() => _ogretmenlerKState();
+}
+
+class _ogretmenlerKState extends State<ogretmenlerK> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          DrawerHeader(
+            child: Align(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    Icons.people_alt,
+                    color: Colors.grey,
+                    size: 50.0,
+                  ),
+                  Text(
+                    "ÖĞRETMENLER",
+                    style: TextStyle(color: Colors.black, fontSize: 25.0),
+                  ),
+                  ListTile(
+                    title: Text('geri dön'),
+                    trailing: Icon(Icons.arrow_back),
+                    onTap: () {
+                      Navigator.push(context,MaterialPageRoute(builder: (context)=>kullanici()));
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
