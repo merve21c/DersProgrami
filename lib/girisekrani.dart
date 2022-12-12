6f7f870742d6933f89e85851c88047b322bd3539
merve21c <>
2022-12-12 16:01:10 +0300

    Update girisekrani.dart

--- lib/girisekrani.dart
+++ lib/girisekrani.dart
@@ -1,8 +1,7 @@
-import 'package:dersprogrami/kayit.dart';
+import 'package:dersprogrami/Kgiris.dart';
+import 'package:dersprogrami/Ygiris.dart';
 import 'package:dersprogrami/kullanici.dart';
-import 'package:dersprogrami/yonetici.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 class girisekrani extends StatefulWidget {
   const girisekrani({Key? key}) : super(key: key);
@@ -12,99 +11,38 @@ class girisekrani extends StatefulWidget {
 }
 
 class _girisekraniState extends State<girisekrani> {
-  Future<User?>loginUsingEmailPassword({required String email, required String password,required BuildContext context}) async{
-    FirebaseAuth auth = FirebaseAuth.instance;
-    User? user;
-    try{
-      UserCredential userCredential=await auth.signInWithEmailAndPassword(email: email, password: password);
-      user = userCredential.user;
-    }on FirebaseAuthException catch(e){
-      if(e.code == "kullanici yok"){
-        print("bu e posta için kullanıcı bulunamadı");
-      }
-    }
-    return user;
-  }
-
   @override
   Widget build(BuildContext context) {
-    final TextEditingController _emailController = TextEditingController();
-    final TextEditingController _passwordController = TextEditingController();
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Form(
+        body: Form(
         child: Padding(
-          padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextField(
-                controller: _emailController,
-                decoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.pink),
-                    ),
-                    labelText: 'E_posta',
-                    hintText: 'E_postanızı giriniz',
-                    prefixIcon: Icon(
-                      Icons.person,
-                      color: Colors.black,
-                    ),
-                    labelStyle: TextStyle(color: Colors.pink),
-                    border: OutlineInputBorder()),
-              ),
-              SizedBox(height: 10.0),
-              TextField(
-                obscureText: true,
-                controller: _passwordController,
-                decoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.pink),
-                    ),
-                    labelText: 'Şifre',
-                    hintText: 'Şifrenizi giriniz',
-                    prefixIcon: Icon(
-                      Icons.key_sharp,
-                      color: Colors.black,
-                    ),
-                    labelStyle: TextStyle(color: Colors.pink),
-                    border: OutlineInputBorder()),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  MaterialButton(
-                    child: Text('Şifremi unuttum'),
-                    onPressed: () {
-                      Navigator.push(context,MaterialPageRoute(builder: (context)=> girisekrani()));
-                    },
-                  )
-                ],
-              ),
-              ElevatedButton(
-                child: Text('Giriş'),
-
-                onPressed: () async {
-                  User? user = await loginUsingEmailPassword(email: _emailController.text, password: _passwordController.text, context: context);
-                  print(user);
-                  if(user != null){
-                    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=> yonetici()));
-                  }
-                },
-              ),
-              ElevatedButton(
-                child: Text('Kayıt Ol'),
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => RegisterPage())); //kayıt
-                },
-              ),
-            ],
-          ),
-        ),
+        padding: const EdgeInsets.only(left: 150.0, right: 20.0),
+    child: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      ElevatedButton(
+        child: Text('Kullanıcı'),
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => Kgiris())); //kayıt
+        },
       ),
+      ElevatedButton(
+        child: Text('Yönetici'),
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => Ygiris())); //kayıt
+        },
+      ),
+
+      ]
+        ),
+    ),
+        ),
     );
   }
 }
