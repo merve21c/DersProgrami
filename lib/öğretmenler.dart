943034f711695d82a2a75e796a491abaa4f25f50
merve21c <mervecanas.21@gmail.com>
2022-12-13 23:48:03 +0300

    Update öğretmenler.dart

--- lib/öğretmenler.dart
+++ lib/öğretmenler.dart
@@ -1,3 +1,4 @@
+import 'package:dersprogrami/Oekle.dart';
 import 'package:dersprogrami/yonetici.dart';
 import 'package:flutter/material.dart';
 
@@ -25,24 +26,39 @@ class _ogretmenlerState extends State<ogretmenler> {
                     size: 50.0,
                   ),
                   Text(
-                    "ÖĞRETMENLER",
+                    "Öğretmenler",
                     style: TextStyle(color: Colors.black, fontSize: 25.0),
-                  ),
-                  ListTile(
-                    title: Text('geri dön'),
-                    trailing: Icon(Icons.arrow_back),
-                    onTap: () {
-                      Navigator.push(context,MaterialPageRoute(builder: (context)=>yonetici()));
-                    },
                   ),
                 ],
               ),
             ),
           ),
+
+          ListTile(
+            title: Text('geri dön'),
+            trailing: Icon(Icons.arrow_back),
+            onTap: () {
+              Navigator.push(context,MaterialPageRoute(builder: (context)=>yonetici()));
+            },
+          ),
+          ListTile(
+            title: Text('M.Mustafa Bahşi'),
+            trailing: Icon(Icons.person),
+            onTap: () {
+              Navigator.push(context,MaterialPageRoute(builder: (context)=>yonetici()));
+            },
+          ),
+          ListTile(
+            title: Text('Ekle'),
+            trailing: Icon(Icons.add),
+            onTap: () {
+              Navigator.push(context,MaterialPageRoute(builder: (context)=>Oekle()));
+            },
+          ),
+
         ],
       ),
     );
   }
 }
-
 
