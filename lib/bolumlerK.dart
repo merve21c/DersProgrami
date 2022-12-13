70418306420c9aca95e9c7daec74c4a91cf2593c
merve21c <mervecanas.21@gmail.com>
2022-12-14 02:14:34 +0300

    Update bolumlerK.dart

--- lib/bolumlerK.dart
+++ lib/bolumlerK.dart
@@ -1,4 +1,5 @@
 import 'package:dersprogrami/kullanici.dart';
+import 'package:dersprogrami/yonetici.dart';
 import 'package:flutter/material.dart';
 
 class bolumlerK extends
@@ -12,36 +13,49 @@ StatefulWidget {
 class _bolumlerKState extends State<bolumlerK> {
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-    child: ListView(
-    children: <Widget>[
-    DrawerHeader(
-    child: Align(
-    child: Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: <Widget>[
-    Icon(
-    Icons.play_lesson,
-    color: Colors.grey,
-    size: 50.0,
-    ),
-    Text(
-    "BÖLÜMLER",
-    style: TextStyle(color: Colors.black, fontSize: 25.0),
-    ),
-    ListTile(
-    title: Text('geri dön'),
-    trailing: Icon(Icons.arrow_back),
-    onTap: () {
-    Navigator.push(context,MaterialPageRoute(builder: (context)=>kullanici()));
-    },
-    ),
-    ],
-    ),
-    ),
-    ),
-    ],
-    ),
+    return Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: Icon(
+              Icons.play_lesson,
+              color: Colors.blueGrey.shade300,
+              size: 300
+          ),
+        ),
+
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Align(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.play_lesson,
+                        color: Colors.grey,
+                        size: 50.0,
+                      ),
+                      Text(
+                        "Dersler",
+                        style: TextStyle(color: Colors.black, fontSize: 25.0),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.keyboard_return),
+                title: Text('Geri dön'),
+                onTap: () {
+                  Navigator.push(context,MaterialPageRoute(builder: (context)=> kullanici()));
+                },
+              ),
+            ],
+          ),
+        )
     );
+
   }
 }
