dbd36ce847be5ea7beff6caffe5680387644bd91
merve21c <mervecanas.21@gmail.com>
2022-12-14 17:48:27 +0300

    Create Osil.dart

---
+++ lib/ekle_sil/Osil.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Osil extends StatefulWidget {
+  const Osil({Key? key}) : super(key: key);
+
+  @override
+  State<Osil> createState() => _OsilState();
+}
+
+class _OsilState extends State<Osil> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
