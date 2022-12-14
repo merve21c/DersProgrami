c4f645a5cd2fe29a991f9a08cc2b978c26863a46
merve21c <mervecanas.21@gmail.com>
2022-12-14 17:48:19 +0300

    Create Dsil.dart

---
+++ lib/ekle_sil/Dsil.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Dsil extends StatefulWidget {
+  const Dsil({Key? key}) : super(key: key);
+
+  @override
+  State<Dsil> createState() => _DsilState();
+}
+
+class _DsilState extends State<Dsil> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
