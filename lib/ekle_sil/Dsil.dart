d889de1e60070b34376de0fd53f0fd48466206b0
merve21c <edaruya5@gmail.com>
2022-12-14 18:26:44 +0300

    Update Dsil.dart

--- lib/ekle_sil/Dsil.dart
+++ lib/ekle_sil/Dsil.dart
@@ -1,3 +1,4 @@
+import 'package:dersprogrami/dersler.dart';
 import 'package:flutter/material.dart';
 
 class Dsil extends StatefulWidget {
@@ -10,6 +11,44 @@ class Dsil extends StatefulWidget {
 class _DsilState extends State<Dsil> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(),
+
+        body: Center(
+          child: Icon(
+              Icons.delete,
+              color: Colors.blueGrey.shade300,
+              size: 300
+          ),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Align(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "Dersler",
+                        style: TextStyle(color: Colors.black, fontSize: 25.0),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.keyboard_return),
+                title: Text('Geri dön'),
+                onTap: () {
+                  Navigator.push(context,MaterialPageRoute(builder: (context)=> dersler()));
+                },
+              ),
+            ],
+          ),
+        )
+    );
+
   }
 }
