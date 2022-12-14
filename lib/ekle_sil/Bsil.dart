f1fa9a9da5a739038ebb549f03ff08c2f9d2f139
merve21c <edaruya5@gmail.com>
2022-12-14 18:26:52 +0300

    Update Bsil.dart

--- lib/ekle_sil/Bsil.dart
+++ lib/ekle_sil/Bsil.dart
@@ -1,3 +1,4 @@
+import 'package:dersprogrami/b%C3%B6l%C3%BCmler.dart';
 import 'package:flutter/material.dart';
 
 class Bsil extends StatefulWidget {
@@ -10,6 +11,45 @@ class Bsil extends StatefulWidget {
 class _BsilState extends State<Bsil> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(),
+
+        body: Center(
+          child: Icon(
+              Icons.delete,
+              color: Colors.blueGrey.shade300,
+              size: 300
+          ),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Align(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+
+                      Text(
+                        "BÖLÜMLER",
+                        style: TextStyle(color: Colors.black, fontSize: 25.0),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.keyboard_return),
+                title: Text('Geri dön'),
+                onTap: () {
+                  Navigator.push(context,MaterialPageRoute(builder: (context)=> bolumler()));
+                },
+              ),
+            ],
+          ),
+        )
+    );
+
   }
 }
