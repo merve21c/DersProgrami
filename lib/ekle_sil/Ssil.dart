34f39b91a105b444950b5b267112f6bfd5ce052f
merve21c <mervecanas.21@gmail.com>
2022-12-14 17:48:53 +0300

    Create Ssil.dart

---
+++ lib/ekle_sil/Ssil.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Ssil extends StatefulWidget {
+  const Ssil({Key? key}) : super(key: key);
+
+  @override
+  State<Ssil> createState() => _SsilState();
+}
+
+class _SsilState extends State<Ssil> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
