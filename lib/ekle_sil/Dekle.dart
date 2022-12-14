fe62b87d7af79e1b876e7aa638bd43b402963abf
merve21c <mervecanas.21@gmail.com>
2022-12-14 17:47:58 +0300

    Create Dekle.dart

---
+++ lib/ekle_sil/Dekle.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Dekle extends StatefulWidget {
+  const Dekle({Key? key}) : super(key: key);
+
+  @override
+  State<Dekle> createState() => _DekleState();
+}
+
+class _DekleState extends State<Dekle> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
