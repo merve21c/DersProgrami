ff521788334f222da83080c25c3fd097a4efc541
merve21c <mervecanas.21@gmail.com>
2022-12-14 18:07:13 +0300

    Update Osil.dart

--- lib/ekle_sil/Osil.dart
+++ lib/ekle_sil/Osil.dart
@@ -1,3 +1,4 @@
+import 'package:dersprogrami/yonetici.dart';
 import 'package:flutter/material.dart';
 
 class Osil extends StatefulWidget {
@@ -10,6 +11,49 @@ class Osil extends StatefulWidget {
 class _OsilState extends State<Osil> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(),
+
+        body: Center(
+          child: Icon(
+              Icons.delete,
+              color: Colors.blueGrey.shade300,
+              size: 300
+          ),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Align(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.delete,
+                        color: Colors.grey,
+                        size: 50.0,
+                      ),
+                      Text(
+                        "Dersler",
+                        style: TextStyle(color: Colors.black, fontSize: 25.0),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.keyboard_return),
+                title: Text('Geri dön'),
+                onTap: () {
+                  Navigator.push(context,MaterialPageRoute(builder: (context)=> yonetici()));
+                },
+              ),
+            ],
+          ),
+        )
+      );
+
   }
 }
