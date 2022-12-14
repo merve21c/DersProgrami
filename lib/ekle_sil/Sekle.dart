ad1591e8673e308d6f96bb9d0cfa1eecc53691f2
merve21c <mervecanas.21@gmail.com>
2022-12-14 17:48:46 +0300

    Create Sekle.dart

---
+++ lib/ekle_sil/Sekle.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Sekle extends StatefulWidget {
+  const Sekle({Key? key}) : super(key: key);
+
+  @override
+  State<Sekle> createState() => _SekleState();
+}
+
+class _SekleState extends State<Sekle> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
