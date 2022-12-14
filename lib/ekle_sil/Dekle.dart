3745042d6d9f3688af86e300ff15e6075c3065dd
merve21c <edaruya5@gmail.com>
2022-12-14 18:26:47 +0300

    Update Dekle.dart

--- lib/ekle_sil/Dekle.dart
+++ lib/ekle_sil/Dekle.dart
@@ -1,3 +1,4 @@
+import 'package:dersprogrami/dersler.dart';
 import 'package:flutter/material.dart';
 
 class Dekle extends StatefulWidget {
@@ -10,6 +11,45 @@ class Dekle extends StatefulWidget {
 class _DekleState extends State<Dekle> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(),
+
+        body: Center(
+          child: Icon(
+              Icons.add,
+              color: Colors.blueGrey.shade300,
+              size: 300
+          ),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Align(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+
+                      Text(
+                        "Dersler",
+                        style: TextStyle(color: Colors.black, fontSize: 25.0),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.keyboard_return),
+                title: Text('Geri dön'),
+                onTap: () {
+                  Navigator.push(context,MaterialPageRoute(builder: (context)=> dersler()));
+                },
+              ),
+            ],
+          ),
+        )
+    );
+
   }
 }
