5b494c62645fe7a7bfed8ddaa281bf23180863e5
merve21c <edaruya5@gmail.com>
2022-12-14 18:26:35 +0300

    Update Ssil.dart

--- lib/ekle_sil/Ssil.dart
+++ lib/ekle_sil/Ssil.dart
@@ -1,3 +1,4 @@
+import 'package:dersprogrami/%C5%9Fubeler.dart';
 import 'package:flutter/material.dart';
 
 class Ssil extends StatefulWidget {
@@ -10,6 +11,45 @@ class Ssil extends StatefulWidget {
 class _SsilState extends State<Ssil> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(),
+
+        body: Center(
+          child: Icon(
+              Icons.delete,
+              color: Colors.blueGrey.shade300,
+              size: 300
+          ),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Align(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+
+                      Text(
+                        "ŞUBELER",
+                        style: TextStyle(color: Colors.black, fontSize: 25.0),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.keyboard_return),
+                title: Text('Geri dön'),
+                onTap: () {
+                  Navigator.push(context,MaterialPageRoute(builder: (context)=> subeler()));
+                },
+              ),
+            ],
+          ),
+        )
+    );
+
   }
 }
