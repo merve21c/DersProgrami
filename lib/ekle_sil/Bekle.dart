1edf565c39f4a129cf20b24c39bd4d2fcca9557b
merve21c <mervecanas.21@gmail.com>
2022-12-14 17:47:49 +0300

    Create Bekle.dart

---
+++ lib/ekle_sil/Bekle.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Bekle extends StatefulWidget {
+  const Bekle({Key? key}) : super(key: key);
+
+  @override
+  State<Bekle> createState() => _BekleState();
+}
+
+class _BekleState extends State<Bekle> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
