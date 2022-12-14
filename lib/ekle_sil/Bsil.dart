37c3f8671e7e5192d4dcd7297128cc4d7437d6ac
merve21c <mervecanas.21@gmail.com>
2022-12-14 18:07:04 +0300

    Create Bsil.dart

---
+++ lib/ekle_sil/Bsil.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Bsil extends StatefulWidget {
+  const Bsil({Key? key}) : super(key: key);
+
+  @override
+  State<Bsil> createState() => _BsilState();
+}
+
+class _BsilState extends State<Bsil> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
