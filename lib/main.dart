4439dd3a247a2b033aebdec739578200ea68e7c4
merve21c <edaruya5@gmail.com>
2022-12-15 21:38:39 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'girisekrani.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+//veri tabanı veya diğer bağlantıları pubcpec.yaml dosyamızın içine ekliyıeuz
+
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-await Firebase.initializeApp();
+await Firebase.initializeApp();     //veri tabanını çalıştırmak için
     runApp(const MyApp());
 }
 class MyApp extends StatelessWidget{
@@ -13,7 +15,7 @@ class MyApp extends StatelessWidget{
 
   Widget build(BuildContext context){
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,  //üst kısımdaki debug yazısını kaldırmak için.
       home: girisekrani (),
     );
   }
