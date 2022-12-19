bd7ffe234d6bd03dc150ca640f7f2db2cb9a4363
merve21c <edaruya5@gmail.com>
2022-12-19 17:45:18 +0300

    Update Bekle.dart

--- lib/ekle_sil/Bekle.dart
+++ lib/ekle_sil/Bekle.dart
@@ -1,5 +1,6 @@
-import 'package:dersprogrami/b%C3%B6l%C3%BCmler.dart';
+import 'package:dersprogrami/servisler/Bekle_service.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class Bekle extends StatefulWidget {
   const Bekle({Key? key}) : super(key: key);
@@ -9,48 +10,94 @@ class Bekle extends StatefulWidget {
 }
 
 class _BekleState extends State<Bekle> {
+  final TextEditingController _bolumadiController = TextEditingController();
+   BekleService _bekleService = BekleService();
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
-        appBar: AppBar(),
-
-        body: Center(
-          child: Icon(
-              Icons.add,
-              color: Colors.blueGrey.shade300,
-              size: 300
-          ),
-        ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              DrawerHeader(
-                child: Align(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-
-
-                      Text(
-                        "BÖLÜMLER",
-                        style: TextStyle(color: Colors.black, fontSize: 25.0),
+      appBar: AppBar(
+        title: Text("Bölüm Ekle"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: size.height * .4,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.blue, width: 2),
+                  borderRadius: BorderRadius.all(Radius.circular(10))),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextField(
+                        controller: _bolumadiController,
+                        maxLines: 2,
+                        decoration: InputDecoration(
+                          hintText: "Bölüm Adı",
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          border: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                        )),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0, right: 8, bottom: 25),
+                      child: InkWell(
+                        onTap: () {
+                          _bekleService
+                              .addBekle(_bolumadiController.text , '')
+                              .then((value) {
+                           Fluttertoast.showToast(
+                                msg: "Bölüm eklendi!",
+                                timeInSecForIosWeb: 5,
+                                toastLength: Toast.LENGTH_SHORT,
+                                gravity: ToastGravity.BOTTOM,
+                                backgroundColor: Colors.grey[600],
+                                textColor: Colors.white,
+                                fontSize: 14);
+                            Navigator.pop(context);
+                          });
+                        },
+                        child: Container(
+                          padding: EdgeInsets.symmetric(vertical: 5),
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.blue, width: 2),
+                              //color: colorPrimaryShade,
+                              borderRadius: BorderRadius.all(Radius.circular(30))),
+                          child: Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: Center(
+                                child: Text(
+                                  "Ekle",
+                                  style: TextStyle(
+                                    color: Colors.blue,
+                                    fontSize: 20,
+                                  ),
+                                )),
+                          ),
+                        ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
+
+
+
+
                 ),
               ),
-              ListTile(
-                leading: Icon(Icons.keyboard_return),
-                title: Text('Geri dön'),
-                onTap: () {
-                  Navigator.push(context,MaterialPageRoute(builder: (context)=> bolumler()));
-                },
-              ),
-            ],
+            ),
           ),
-        )
+        ],
+      ),
     );
-
   }
 }
