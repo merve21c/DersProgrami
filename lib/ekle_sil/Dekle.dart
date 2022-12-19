8c2fea331d1f1388f2294b2cfa1cd5b81a54fbcd
merve21c <edaruya5@gmail.com>
2022-12-19 16:45:13 +0300

    Update Dekle.dart

--- lib/ekle_sil/Dekle.dart
+++ lib/ekle_sil/Dekle.dart
@@ -1,55 +1,135 @@
-import 'package:dersprogrami/dersler.dart';
+import 'package:dersprogrami/servisler/Dekle_service.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class Dekle extends StatefulWidget {
-  const Dekle({Key? key}) : super(key: key);
+  const Dekle({Key? key, required int dersId, required String dersadi, required int dersSaati}) : super(key: key);
 
   @override
   State<Dekle> createState() => _DekleState();
 }
 
 class _DekleState extends State<Dekle> {
+  final TextEditingController _dersIdController = TextEditingController();
+  final TextEditingController _dersadiController = TextEditingController();
+  final TextEditingController _dersSaatiController = TextEditingController();
+       DekleService _dekleService = DekleService();
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return Scaffold(
-        appBar: AppBar(),
-
-        body: Center(
-          child: Icon(
-              Icons.add,
-              color: Colors.blueGrey.shade300,
-              size: 300
-          ),
-        ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              DrawerHeader(
-                child: Align(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-
-                      Text(
-                        "Dersler",
-                        style: TextStyle(color: Colors.black, fontSize: 25.0),
+      appBar: AppBar(
+        title: Text("Öğretmen ekle"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: size.height * .4,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.blue, width: 2),
+                  borderRadius: BorderRadius.all(Radius.circular(10))),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextField(
+                        controller: _dersIdController,
+                        maxLines: 2,
+                        decoration: InputDecoration(
+                          hintText: "Ders Id",
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          border: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                        )),
+                    TextField(
+                        controller: _dersadiController,
+                        maxLines: 2,
+                        decoration: InputDecoration(
+                          hintText: "Ders Adı",
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          border: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                        )),
+                    TextField(
+                        controller: _dersSaatiController,
+                        maxLines: 2,
+                        decoration: InputDecoration(
+                          hintText: "Ders Saati",
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          border: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                        )),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0, right: 8, bottom: 25),
+                      child: InkWell(
+                        onTap: () {
+                          _dekleService
+                              .addDekle(_dersIdController.hashCode , _dersadiController.text , _dersSaatiController.hashCode ,'')
+                              .then((value) {
+                            Fluttertoast.showToast(
+                                msg: "Ders Bilgileri eklendi!",
+                                timeInSecForIosWeb: 5,
+                                toastLength: Toast.LENGTH_SHORT,
+                                gravity: ToastGravity.BOTTOM,
+                                backgroundColor: Colors.grey[600],
+                                textColor: Colors.white,
+                                fontSize: 14);
+                            Navigator.pop(context);
+                          });
+                        },
+                        child: Container(
+                          padding: EdgeInsets.symmetric(vertical: 5),
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.blue, width: 2),
+                              //color: colorPrimaryShade,
+                              borderRadius: BorderRadius.all(Radius.circular(30))),
+                          child: Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: Center(
+                                child: Text(
+                                  "Ekle",
+                                  style: TextStyle(
+                                    color: Colors.blue,
+                                    fontSize: 20,
+                                  ),
+                                )),
+                          ),
+                        ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
+
+
+
+
                 ),
               ),
-              ListTile(
-                leading: Icon(Icons.keyboard_return),
-                title: Text('Geri dön'),
-                onTap: () {
-                  Navigator.push(context,MaterialPageRoute(builder: (context)=> dersler()));
-                },
-              ),
-            ],
+            ),
           ),
-        )
+        ],
+      ),
     );
-
   }
 }
