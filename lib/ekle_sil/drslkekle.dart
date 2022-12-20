f98eadfb948b0c91bc46c95fe39eb9de6c77efe1
merve21c <mervecanas.21@gmail.com>
2022-12-20 17:11:30 +0300

    Update drslkekle.dart

---
+++ lib/ekle_sil/drslkekle.dart
@@ -0,0 +1,103 @@
+import 'package:dersprogrami/servisler/dersklikekle_service.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class derslikekle extends StatefulWidget {
+  const derslikekle({Key? key}) : super(key: key);
+
+  @override
+  State<derslikekle> createState() => _derslikekleState();
+}
+
+class _derslikekleState extends State<derslikekle> {
+  final TextEditingController _derslikController = TextEditingController();
+  derslikekleService _derslikekleService = derslikekleService();
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return  Scaffold(
+      appBar: AppBar(
+        title: Text("Derslik"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: size.height * .4,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.blue, width: 2),
+                  borderRadius: BorderRadius.all(Radius.circular(10))),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextField(
+                        controller: _derslikController,
+                        maxLines: 2,
+                        decoration: InputDecoration(
+                          hintText: "Derslik",
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          border: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                        )),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0, right: 8, bottom: 25),
+                      child: InkWell(
+                        onTap: () {
+                         _derslikekleService
+                              .addderslikekle(_derslikController.text,'')
+                              .then((value) {
+                            Fluttertoast.showToast(
+                                msg: "Derslik eklendi!",
+                                timeInSecForIosWeb: 5,
+                                toastLength: Toast.LENGTH_SHORT,
+                                gravity: ToastGravity.BOTTOM,
+                                backgroundColor: Colors.grey[600],
+                                textColor: Colors.white,
+                                fontSize: 14);
+                            Navigator.pop(context);
+                          });
+                        },
+                        child: Container(
+                          padding: EdgeInsets.symmetric(vertical: 5),
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.blue, width: 2),
+                              //color: colorPrimaryShade,
+                              borderRadius: BorderRadius.all(Radius.circular(30))),
+                          child: Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: Center(
+                                child: Text(
+                                  "Ekle",
+                                  style: TextStyle(
+                                    color: Colors.blue,
+                                    fontSize: 20,
+                                  ),
+                                )),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+
+
+
+
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
