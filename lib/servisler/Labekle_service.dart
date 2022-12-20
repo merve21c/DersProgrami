c2c9f48a0fff95fa6a8a454f5ad6685eadef8d3b
merve21c <mervecanas.21@gmail.com>
2022-12-20 17:11:07 +0300

    Update Labekle_service.dart

---
+++ lib/servisler/Labekle_service.dart
@@ -0,0 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dersprogrami/ekle_sil/Labekle.dart';
+
+class LabekleService {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+// veri ekleme fonksiyonu
+  Future<Labekle> addLabekle(String laboratuvar , String text) async {
+    var ref = _firestore.collection(
+        "Labaratuvar"); //koleksiyonumuzun ismini yazıyoruz .
+    await ref.add({
+
+      'laboratuvar':laboratuvar,
+    });
+    return Labekle();
+  }
+
+  //veri gösterme fonksiyonu
+  //sayfadaki verilerin anlık değişimini göstermek için stream kullanıyoruz
+  Stream<QuerySnapshot> getLabekle() {
+    var ref = _firestore.collection("Şubeler").snapshots();
+    return ref;
+  }
+}
