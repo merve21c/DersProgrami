8fd23d839e840a9702d9dd8bb7b8d8b9827a5323
merve21c <mervecanas.21@gmail.com>
2022-12-22 20:43:22 +0300

    Update O_list.dart

---
+++ lib/O_list.dart
@@ -0,0 +1,128 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dersprogrami/servisler/Oekle_service.dart';
+import 'package:flutter/material.dart';
+
+class Olist extends StatefulWidget {
+  @override
+  _OlistState createState() => _OlistState();
+}
+
+class _OlistState extends State<Olist> {
+  OekleService _oekleService = OekleService();
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+
+    return StreamBuilder<QuerySnapshot>(
+      stream: _oekleService.getOekle(),
+      builder: (context, snapshot) {
+        return !snapshot.hasData
+            ? CircularProgressIndicator()
+            : ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              DocumentSnapshot mypost = snapshot.data!.docs[index];
+
+              Future<void> _showChoiseDialog(BuildContext context) {
+                return showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                          title: Text(
+                            "Silmek istediğinize emin misiniz?",
+                            textAlign: TextAlign.center,
+                          ),
+                          shape: RoundedRectangleBorder(
+                              borderRadius:
+                              BorderRadius.all(Radius.circular(8.0))),
+                          content: Container(
+                              height: 30,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                              ),
+                              child: Row(
+                                mainAxisAlignment:
+                                MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  GestureDetector(
+                                    onTap: () {
+                                      _oekleService
+                                          .removeOekle(mypost.id);
+                                      Navigator.pop(context);
+                                    },
+                                    child: Text(
+                                      "Evet",
+                                      style: TextStyle(
+                                          color: Colors.blue,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                  GestureDetector(
+                                    onTap: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: Text(
+                                      "Vazgeç",
+                                      style: TextStyle(
+                                          color: Colors.blue,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                ],
+                              )));
+                    });
+              }
+
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: InkWell(
+                  onTap: () {
+                    _showChoiseDialog(context);
+                  },
+                  child: Container(
+                    height: size.height * .3,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(color: Colors.blue, width: 2),
+                        borderRadius:
+                        BorderRadius.all(Radius.circular(10))),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("${mypost['adisoyadi']}",
+                            style: TextStyle(fontSize: 16),
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            "${mypost['unvan']}",
+                            style: TextStyle(fontSize: 16),
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            "${mypost['ders']}",
+                            style: TextStyle(fontSize: 16),
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            });
+      },
+    );
+  }
+}
