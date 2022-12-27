bb5110be33b0936098f12aac3002d9eae1aecb2d
merve21c <mervecanas.21@gmail.com>
2022-12-27 17:37:51 +0300

    Create sosyalhizmetler.dart

---
+++ lib/sosyalhizmetler.dart
@@ -0,0 +1,39 @@
+import 'package:dersprogrami/B%C3%B6l%C3%BCm_list.dart';
+import 'package:flutter/material.dart';
+
+import 'ekle_sil/Bekle.dart';
+
+class Sosyalhizmet extends StatefulWidget {
+  const Sosyalhizmet({Key? key}) : super(key: key);
+
+  @override
+  State<Sosyalhizmet> createState() => _SosyalhizmetState();
+}
+
+class _SosyalhizmetState extends State<Sosyalhizmet> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+          title: Text("Sosyal Hizmetler"),
+          actions: <Widget>[
+            IconButton( icon: Icon(Icons.people_alt_rounded),
+                onPressed: (){
+                  Navigator.push(context,MaterialPageRoute(builder: (context)=>Sosyalhizmet()));   //sayfalar arası geçiş
+                }
+            )
+          ]
+      ),
+      body: Center(
+        child: bolumlist(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) =>Bekle()));
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
