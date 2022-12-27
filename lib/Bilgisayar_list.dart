fb148d13366345b0077c339d61c9e7f2a33789d9
merve21c <mervecanas.21@gmail.com>
2022-12-27 17:37:33 +0300

    Create Bilgisayar_list.dart

---
+++ lib/Bilgisayar_list.dart
@@ -0,0 +1,151 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dersprogrami/servisler/veriler.dart';
+import 'package:flutter/material.dart';
+
+class bilgisayarlist extends StatefulWidget {
+  const bilgisayarlist({Key? key}) : super(key: key);
+
+  @override
+  State<bilgisayarlist> createState() => _bilgisayarlistState();
+}
+
+class _bilgisayarlistState extends State<bilgisayarlist> {
+  VeriekleService _veriekleService =VeriekleService();
+  @override
+  Widget build(BuildContext context) {
+  var size = MediaQuery.of(context).size;
+
+    return StreamBuilder<QuerySnapshot>(
+      stream: _veriekleService.getVeriekle(),
+      builder: (context, snapshot) {
+        return !snapshot.hasData
+            ? CircularProgressIndicator()
+            : ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              DocumentSnapshot mypost = snapshot.data!.docs[index];
+
+              Future<void> _showChoiseDialog(BuildContext context) {
+                return showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                          title: Text(
+                            "Silmek istediğinize emin misiniz?",
+                            textAlign: TextAlign.center,
+                          ),
+                          shape: RoundedRectangleBorder(
+                              borderRadius:
+                              BorderRadius.all(Radius.circular(8.0))),
+                          content: Container(
+                              height: 30,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                              ),
+                              child: Row(
+                                mainAxisAlignment:
+                                MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  GestureDetector(
+                                    onTap: () {
+                                      _veriekleService
+                                          .removeVeriekle(mypost.id);
+                                      Navigator.pop(context);
+                                    },
+                                    child: Text(
+                                      "Evet",
+                                      style: TextStyle(
+                                          color: Colors.blue,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                  GestureDetector(
+                                    onTap: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: Text(
+                                      "Vazgeç",
+                                      style: TextStyle(
+                                          color: Colors.blue,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                ],
+                              )
+                          )
+                      );
+                    }
+                );
+              }
+
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: InkWell(
+                  onTap: () {
+                    _showChoiseDialog(context);
+                  },
+                  child: Container(
+                    height: size.height * .4,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        border: Border.all(color: Colors.blue, width: 2),
+                        borderRadius:
+                        BorderRadius.all(Radius.circular(10))),
+                    child: Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("${mypost['bolumadiID']}",
+                            style: TextStyle(fontSize: 16),
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text("${mypost['gun']}",
+                            style: TextStyle(fontSize: 16),
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text("${mypost['dersadi']}",
+                            style: TextStyle(fontSize: 16),
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text("${mypost['dersOgretmeni']}",
+                            style: TextStyle(fontSize: 16),
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text("${mypost['dersSaati']}",
+                            style: TextStyle(fontSize: 16),
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text("${mypost['amfi']}",
+                            style: TextStyle(fontSize: 16),
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            });
+      },
+    );
+  }
+}
