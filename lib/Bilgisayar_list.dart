735cf834e9647f16c779454545362aefe3185c07
merve21c <edaruya5@gmail.com>
2022-12-29 01:54:54 +0300

    Update Bilgisayar_list.dart

--- lib/Bilgisayar_list.dart
+++ lib/Bilgisayar_list.dart
@@ -1,5 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:dersprogrami/servisler/bilgisayarE_service.dart';
+import 'package:dersprogrami/servisler/bilgisayardersP_service.dart';
 import 'package:flutter/material.dart';
 
 class bilgisayarlist extends StatefulWidget {
