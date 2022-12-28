25923e63f6b7466a32d3712b34b9413bd88fccdd
merve21c <canannur15@gmail.com>
2022-12-28 18:23:19 +0300

    Update Lojistik.dart

--- lib/Lojistik.dart
+++ lib/Lojistik.dart
@@ -1,6 +1,4 @@
-import 'package:dersprogrami/B%C3%B6l%C3%BCm_list.dart';
 import 'package:flutter/material.dart';
-import 'ekle_sil/Bekle.dart';
 
 class Lojistik extends StatefulWidget {
   const Lojistik({Key? key}) : super(key: key);
@@ -12,27 +10,6 @@ class Lojistik extends StatefulWidget {
 class _LojistikState extends State<Lojistik> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-          title: Text("Lojistik"),
-          actions: <Widget>[
-            IconButton( icon: Icon(Icons.fire_truck_sharp),
-                onPressed: (){
-                  Navigator.push(context,MaterialPageRoute(builder: (context)=>Lojistik()));   //sayfalar arası geçiş
-                }
-            )
-          ]
-      ),
-      body: Center(
-        child: bolumlist(),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(context,
-              MaterialPageRoute(builder: (context) =>Bekle()));
-        },
-        child: Icon(Icons.add),
-      ),
-    );
+    return Container();
   }
 }
