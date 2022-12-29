c34213ecf063872ba1a52f6d077d1e4f2a633e34
merve21c <mervecanas.21@gmail.com>
2022-12-29 03:08:45 +0300

    Create muhasebedPlist.dart

---
+++ lib/kullanıcıdersp/muhasebedPlist.dart
@@ -0,0 +1,93 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dersprogrami/servisler/muhasebe_service.dart';
+import 'package:flutter/material.dart';
+
+class muhasebedPlist extends StatefulWidget {
+  const muhasebedPlist({Key? key}) : super(key: key);
+
+  @override
+  State<muhasebedPlist> createState() => _muhasebedPlistState();
+}
+
+class _muhasebedPlistState extends State<muhasebedPlist> {
+  MuhasebeService _muhasebeService=MuhasebeService();
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+
+    return StreamBuilder<QuerySnapshot>(
+      stream: _muhasebeService.getMuhasebeDersPekle(),
+      builder: (context, snapshot) {
+        return !snapshot.hasData
+            ? CircularProgressIndicator()
+            : ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              DocumentSnapshot mypost = snapshot.data!.docs[index];
+
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: size.height * .4,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(color: Colors.blue, width: 2),
+                      borderRadius:
+                      BorderRadius.all(Radius.circular(10))),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("${mypost['bolumadiID']}",
+                          style: TextStyle(fontSize: 16),
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text("${mypost['gun']}",
+                          style: TextStyle(fontSize: 16),
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text("${mypost['dersadi']}",
+                          style: TextStyle(fontSize: 16),
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text("${mypost['dersOgretmeni']}",
+                          style: TextStyle(fontSize: 16),
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text("${mypost['dersSaati']}",
+                          style: TextStyle(fontSize: 16),
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text("${mypost['amfi']}",
+                          style: TextStyle(fontSize: 16),
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            });
+      },
+    );
+  }
+}
