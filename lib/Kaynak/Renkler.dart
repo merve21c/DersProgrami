a352b9601822f4db2f387b156010dac3a3b6884c
merve21c <mervecanas.21@gmail.com>
2022-12-30 05:12:14 +0300

    Create Renkler.dart

---
+++ lib/Kaynak/Renkler.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class KullaniciColors{
+  static Color mainColor = Colors.grey.shade300;
+  static Color pink = Colors.pinkAccent.shade100;
+  static Color white = Colors.white;
+  static Color black =Colors.black;
+  static Color grey = Colors.grey.shade700;
+  static Color purple = Colors.purpleAccent.shade100;
+
+  KullaniciColors._();
+}
