2ddeede833405285bcc705243b93fe812e41db46
merve21c <mervecanas.21@gmail.com>
2022-12-30 05:17:06 +0300

    Create String.dart

---
+++ lib/Kaynak/String.dart
@@ -0,0 +1,48 @@
+class KullaniciText{
+
+  static String emptyText = "Eksik bilgi...";
+  static String errorText = "Beklenmedik bir hata oluştu...";
+
+  static String loginText = "Giriş yap";
+  static String registerText = "Kayıt ol";
+
+  //Kayıt ol
+  static String username = "Kullanıcı adı";
+  static String email = "Kullanıcı maili";
+  static String password = "Parola";
+  static String registerQuestionGun ='Gunler';
+  static String registerQuestionDers = "Gireceği Dersler";
+
+  static String Pazartesi = "Pazartesi";
+  static String Sali = "Sali";
+  static String Carsamba = "Carsamba";
+  static String Persembe = "Persembe";
+  static String Cuma = "Cuma";
+
+  //dersler
+  static String NesneYonelimli ="NesneYonelimli";
+  static String MobilProgramlama ="MobilProgramlama";
+  static String Girisimcilik ="Girisimcilik";
+  static String  GorselProgramlama="GorselProgramlama";
+  static String  SunucuIsletim="SunucuIsletim";
+  static String  GrafikTasarim=" GrafikTasarim";
+
+
+  //Giriş
+  static String loginNoAccountText = "Kullanıcı yok,Kayıt Ol!";
+  static String loginWrongPasswordText = "Yanlış parola, bir daha düşün...";
+  static String loginWrongEmailText = "Böyle mail mi olur?";
+  static String crashlytics = "Firebase Crashlytics";
+
+  //Kullanıcı sayfası
+  static String homeText = "Anasayfa";
+  static String profileText = "Profilim";
+  static String logoutText = "Çıkış yap";
+  static String dersList = "Ders Listesi:";
+
+  //Ekle Unvan
+  static String addUnvanText = "Unvan Ekle";
+  static String addDersProgramiTitle = "Ders Programı?";
+
+  KullaniciText._();
+}
