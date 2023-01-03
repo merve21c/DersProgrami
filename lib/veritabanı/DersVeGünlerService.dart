4e31c8c7f5a808aa2d56ad8b94fe9c6d761b80be
merve21c <mervecanas.21@gmail.com>
2023-01-03 21:09:59 +0300

    Create DersVeGünlerService.dart

---
+++ lib/veritabanı/DersVeGünlerService.dart
@@ -0,0 +1,49 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../Kullanıcı/DersVeMüsaitGün.dart';
+
+class DersVeGunService{
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  Future<DersVeMusaitGun> addDersVeGun(
+      String Pazartesi,
+      String Sali,
+      String Carsamba,
+      String Persembe,
+      String Cuma,
+
+      String Nesne ,
+      String Programlama,
+      String Mobil,
+      String Gorsel,
+      String Veri,
+      )async {
+    var ref = _firestore.collection(
+        "Kullanıcı"); //koleksiyonumuzun ismini yazıyoruz .
+    await ref.add({
+      'Pazartesi': Pazartesi,
+      'Sali': Sali,
+      'Carsamba': Carsamba,
+      'Persembe': Persembe,
+      'Cuma': Cuma,
+
+      'Nesne': Nesne,
+      'Programlama': Nesne,
+      'Mobil': Mobil,
+      'Gorsel': Gorsel,
+      'Veri': Veri,
+
+    });
+    return DersVeMusaitGun();
+  }
+  Stream<QuerySnapshot> getDersVeGun() {
+    var ref = _firestore.collection("Kullanıcı").snapshots();
+    return ref;
+  }
+  Future<void> removeDersVeGun(String docId) {
+    var ref = _firestore.collection('Kullanıcı').doc(docId).delete();
+
+    return ref;
+
+  }
+
+}
