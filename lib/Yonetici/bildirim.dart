f13c282bdfb24db659e9bac8bf3e2e5ba140d0ae
merve21c <mervecanas.21@gmail.com>
2023-01-05 16:53:44 +0300

    Create bildirim.dart

---
+++ lib/Yonetici/bildirim.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class bildirim extends StatefulWidget {
+  const bildirim({Key? key}) : super(key: key);
+
+  @override
+  State<bildirim> createState() => _bildirimState();
+}
+
+class _bildirimState extends State<bildirim> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
