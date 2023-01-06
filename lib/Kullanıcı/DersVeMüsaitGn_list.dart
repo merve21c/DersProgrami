ee14cd0467a2484764143989f090f1e55555d830
merve21c <mervecanas.21@gmail.com>
2023-01-07 02:56:51 +0300

    Update DersVeMüsaitGn_list.dart

--- lib/Kullanıcı/DersVeMüsaitGn_list.dart
+++ lib/Kullanıcı/DersVeMüsaitGn_list.dart
@@ -1,4 +1,3 @@
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dersprogrami/Kaynak/Renkler.dart';
 import 'package:dersprogrami/Kaynak/String.dart';
@@ -14,111 +13,121 @@ class DersVeMusaitGunList extends StatefulWidget {
 }
 
 class _DersVeMusaitGunListState extends State<DersVeMusaitGunList> {
-DersVeGunService _dersVeGunService = DersVeGunService();
+  DersVeGunService _dersVeGunService = DersVeGunService();
+
   @override
   Widget build(BuildContext context) {
     return _body();
   }
 
-StreamBuilder _body() {
-  return StreamBuilder<QuerySnapshot>(
-    stream: _dersVeGunService.getDersVeGun(),
-    builder: (context, snaphot) {
-      return !snaphot.hasData
-          ? const BeklemeSayfasi()
-          : snaphot.data != null
-          ? _mainBody(snaphot)
-          : const BeklemeSayfasi();
-    },
-  );
-}
 
-ListView _mainBody(AsyncSnapshot<QuerySnapshot<Object?>> snaphot) {
-  return ListView.builder(
-      itemCount: snaphot.data?.docs.length ?? 0,
-      itemBuilder: (context, index) {
-        DocumentSnapshot mypost = snaphot.data!.docs[index];
+  StreamBuilder _body() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _dersVeGunService.getDersVeGun(),
+      builder: (context, snaphot) {
+        return !snaphot.hasData
+            ? const BeklemeSayfasi()
+            : snaphot.data != null
+                ? _mainBody(snaphot)
+                : const BeklemeSayfasi();
+      },
+    );
+  }
 
-        Future<void> _showChoiseDialog(BuildContext context) {
-          return showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return _alertDialog(mypost);
-              });
-        }
+  ListView _mainBody(AsyncSnapshot<QuerySnapshot<Object?>> snaphot) {
+    return ListView.builder(
+        itemCount: snaphot.data?.docs.length ?? 0,
+        itemBuilder: (context, index) {
+          DocumentSnapshot mypost = snaphot.data!.docs[index];
 
-        return Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: InkWell(
-            onTap: () {
-              _showChoiseDialog(context);
-            },
-            child: _dersvegunlistContainer(mypost),
+
+          Future<void> _showChoiseDialog(BuildContext context) {
+            return showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return _alertDialog(mypost);
+                });
+          }
+
+          return Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: InkWell(
+              onTap: () {
+                _showChoiseDialog(context);
+              },
+              child: _dersvegunlistContainer(mypost),
+            ),
+          );
+        });
+  }
+
+  Container _dersvegunlistContainer(DocumentSnapshot mypost) {
+    return Container(
+      decoration: BoxDecoration(
+        color: KullaniciColors.white,
+        borderRadius: const BorderRadius.all(Radius.circular(20)),
+      ),
+      child: _dietListColumn(mypost),
+    );
+  }
+
+  Padding _dietListColumn(DocumentSnapshot mypost) {
+
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            KullaniciText.sayfaText,
+            style: const TextStyle(fontSize: 16),
           ),
-        );
-      });
-}
+          const Divider(),
+          const SizedBox(
+            height: 10,
+          ),
 
-Container _dersvegunlistContainer(DocumentSnapshot mypost) {
-  return Container(
-    decoration: BoxDecoration(
-      color:KullaniciColors.white,
-      borderRadius: const BorderRadius.all(Radius.circular(20)),
-    ),
-    child: _dietListColumn(mypost),
-  );
-}
+          Text(
+              mypost["Pazartesi"] == "true" ? "Pazartesi" : "Boş"
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            mypost["Sali"] == "true" ? "Salı" : "Boş"
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            mypost["Carsamba"] == "true" ? "Çarşamba" : "Boş"
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+              mypost["Persembe"] == "true" ? "Perşembe" : "Boş"
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+              mypost["Cuma"] == "true" ? "Cuma" : "Boş"
 
-Padding _dietListColumn(DocumentSnapshot mypost) {
-  return Padding(
-    padding: const EdgeInsets.all(20.0),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          KullaniciText.sayfaText,
-          style: const TextStyle(fontSize: 16),
-        ),
-        const Divider(),
-        const SizedBox(
-          height: 10,
-        ),
-        Text(
-          "${mypost['Pazartesi']}",
-        ),
-        const SizedBox(
-          height: 10,
-        ), Text(
-          "${mypost['Sali']}",
-        ),
-        const SizedBox(
-          height: 10,
-        ), Text(
-          "${mypost['Carsamba']}",
-        ),
-        const SizedBox(
-          height: 10,
-        ), Text(
-          "${mypost['Persembe']}",
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        Text(
-          "${mypost['Cuma']}",
-        ),
-        const SizedBox(
-          height: 10,
-        ), Text(
-          "${mypost['Nesne']}",
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-      ],
-    ),
-  );
-}
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+              mypost["Nesne"] == "true" ? "Nesne" : "Boş"
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+        ],
+      ),
+    );
+  }
 }
 
 AlertDialog _alertDialog(DocumentSnapshot mypost) {
@@ -139,9 +148,6 @@ AlertDialog _alertDialog(DocumentSnapshot mypost) {
 Row _alertDialogContent(DocumentSnapshot mypost) {
   return Row(
     mainAxisAlignment: MainAxisAlignment.spaceAround,
-    children: <Widget>[
-    ],
+    children: <Widget>[],
   );
 }
-
-
