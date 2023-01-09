66766a097e6486ba5dddf634c2d3709dd9f620e6
merve21c <mervecanas.21@gmail.com>
2023-01-09 20:45:06 +0300

    Create EklemeService.dart

---
+++ lib/veritabanı/EklemeService.dart
@@ -0,0 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dersprogrami/Yonetici/ProgramE.dart';
+
+class EkleService {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<Program> addProgram(String adsoyad,String Unvan,String Ders,String Lab,String Saat ) async {
+    var ref =
+        _firestore.collection("Öğretmen"); //koleksiyonumuzun ismini yazıyoruz .
+    await ref.add({
+      'adsoyad': adsoyad,
+      'Unvan':Unvan,
+      'Ders':Ders,
+      'Lab':Lab,
+      'Saat':Saat,
+    });
+    return Program();
+  }
+
+  Stream<QuerySnapshot> getProgram() {
+    var ref = _firestore.collection("Öğretmen").snapshots();
+    return ref;
+  }
+
+  Future<void> removeProgram(String docId) {
+    var ref = _firestore.collection('Öğretmen').doc(docId).delete();
+
+    return ref;
+  }
+}
