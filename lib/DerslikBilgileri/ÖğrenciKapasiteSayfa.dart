6569603ceb4059b7f1cc4939cdcbfdbb423d5ddf
merve21c <mervecanas.21@gmail.com>
2023-01-10 05:28:30 +0300

    Delete ÖğrenciKapasiteSayfa.dart

--- lib/DerslikBilgileri/ÖğrenciKapasiteSayfa.dart
+++
@@ -1,9 +0,0 @@
-class OgrenciKapasiteSayfa extends Cubit<void> {
-  OgrenciKapasiteSayfa():super(0);
-
-  var OgrenciDepo = OgrenciKapasiteDepo();
-
-  Future<void> kayit(String derslik, String kapasite) async {
-    await OgrenciDepo.OgrenciKayit(derslik, kapasite);
-  }
-}
