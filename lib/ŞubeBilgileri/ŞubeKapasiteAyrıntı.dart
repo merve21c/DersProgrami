303e45ce773aafc420b9b7d279dd12da7fd6886b
merve21c <mervecanas.21@gmail.com>
2023-01-10 05:26:27 +0300

    Delete ŞubeKapasiteAyrıntı.dart

--- lib/ŞubeBilgileri/ŞubeKapasiteAyrıntı.dart
+++
@@ -1,9 +0,0 @@
-class KisiDetayCubit extends Cubit<void> {
-  KisiDetayCubit():super(0);
-
-  var krepo = KisilerDaoRepository();
-
-  Future<void> guncelle(kisi_id ,String subead, String dersbaslangic,String dersbitis, String derssuresi, String teneffussaat, String ogletatili ) async {
-    await krepo.kisiGuncelle(kisi_id, subead,  dersbaslangic, dersbitis, derssuresi, teneffussaat,  ogletatili );
-  }
-}
