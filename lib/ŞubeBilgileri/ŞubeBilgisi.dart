b6f8cc46c97050256d579f55926d7be7707aea4c
merve21c <mervecanas.21@gmail.com>
2023-01-10 04:57:55 +0300

    Create ŞubeBilgisi.dart

---
+++ lib/ŞubeBilgileri/ŞubeBilgisi.dart
@@ -0,0 +1,29 @@
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const subebilgi());
+}
+
+class subebilgi extends StatelessWidget {
+  const subebilgi({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => KisiKayitCubit()),
+        BlocProvider(create: (context) => KisiDetayCubit()),
+        BlocProvider(create: (context) => AnasayfaCubit()),
+      ],
+      child: MaterialApp(
+        title: 'şube bilgileri',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const Anasayfa(),
+      ),
+    );
+  }
+}
