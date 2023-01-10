b0e61291b7acd8c81ce196026f5cb40b94b61aeb
merve21c <mervecanas.21@gmail.com>
2023-01-10 05:27:14 +0300

    Delete ÖğretmenKapasiteSayfası.dart

--- lib/MüsaitGün/ÖğretmenKapasiteSayfası.dart
+++
@@ -1,9 +0,0 @@
-lass KisiKayitCubit extends Cubit<void> {
-KisiKayitCubit():super(0);
-
-var krepo = KisilerDaoRepository();
-
-Future<void> kayit(String kisi_ad, String kisi_tel) async {
-  await krepo.kisiKayit(kisi_ad, kisi_tel);
-}
-}
