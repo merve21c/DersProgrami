d9f1aebc05b45a9df397a0b077dbf718607db005
merve21c <mervecanas.21@gmail.com>
2023-01-10 05:00:42 +0300

    Create DersGüncelle.dart

---
+++ lib/Dersbilgisi/DersGüncelle.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+
+class OgrenciGuncelle extends StatefulWidget {
+  final String id;
+  OgrenciGuncelle ({Key? key, required this.id}) : super(key: key);
+
+  @override
+  _OgrenciGuncelleState createState() => _OgrenciGuncelleState();
+}
+
+class _OgrenciGuncelleState extends State<OgrenciGuncelle > {
+  final _formKey = GlobalKey<FormState>();
+
+
+  CollectionReference students =
+  FirebaseFirestore.instance.collection('students');
+
+  Future<void> updateUser(id, name, email, password) {
+    return students
+        .doc(id)
+        .update({'kod': name, 'ders adı': email, 'ders saati': password})
+        .then((value) => print("User Updated"))
+        .catchError((error) => print("Failed to update user: $error"));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Update Student"),
+      ),
+      body: Form(
+          key: _formKey,
+          // Getting Specific Data by ID
+          child: FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+            future: FirebaseFirestore.instance
+                .collection('students')
+                .doc(widget.id)
+                .get(),
+            builder: (_, snapshot) {
+              if (snapshot.hasError) {
+                print('Something Went Wrong');
+              }
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              var data = snapshot.data!.data();
+              var name = data!['kod'];
+              var email = data['ders adı'];
+              var password = data['ders saati'];
+              return Padding(
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+                child: ListView(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 10.0),
+                      child: TextFormField(
+                        initialValue: name,
+                        autofocus: false,
+                        onChanged: (value) => name = value,
+                        decoration: InputDecoration(
+                          labelText: 'kod: ',
+                          labelStyle: TextStyle(fontSize: 20.0),
+                          border: OutlineInputBorder(),
+                          errorStyle:
+                          TextStyle(color: Colors.redAccent, fontSize: 15),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please Enter Name';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 10.0),
+                      child: TextFormField(
+                        initialValue: email,
+                        autofocus: false,
+                        onChanged: (value) => email = value,
+                        decoration: InputDecoration(
+                          labelText: 'ders adı: ',
+                          labelStyle: TextStyle(fontSize: 20.0),
+                          border: OutlineInputBorder(),
+                          errorStyle:
+                          TextStyle(color: Colors.redAccent, fontSize: 15),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please Enter Email';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 10.0),
+                      child: TextFormField(
+                        initialValue: password,
+                        autofocus: false,
+                        onChanged: (value) => password = value,
+
+                        decoration: InputDecoration(
+                          labelText: 'ders saati: ',
+                          labelStyle: TextStyle(fontSize: 20.0),
+                          border: OutlineInputBorder(),
+                          errorStyle:
+                          TextStyle(color: Colors.redAccent, fontSize: 15),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please Enter Password';
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    Container(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {
+                              // Validate returns true if the form is valid, otherwise false.
+                              if (_formKey.currentState!.validate()) {
+                                updateUser(widget.id, name, email, password);
+                                Navigator.pop(context);
+                              }
+                            },
+                            child: Text(
+                              'güncelle',
+                              style: TextStyle(fontSize: 18.0),
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () => {},
+                            child: Text(
+                              'sıfırla',
+                              style: TextStyle(fontSize: 18.0),
+                            ),
+                            style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.blueGrey),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              );
+            },
+          )),
+    );
+  }
+}
