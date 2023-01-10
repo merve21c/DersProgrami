2f0841930bb4e87b2a1e38519afde8fd88c0e48b
merve21c <mervecanas.21@gmail.com>
2023-01-10 04:59:16 +0300

    Create ÖğretmenAnaSayfa.dart

---
+++ lib/MüsaitGün/ÖğretmenAnaSayfa.dart
@@ -0,0 +1,23 @@
+class MyAppp extends StatelessWidget {
+  const MyAppp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => KisiKayitCubit()),
+        BlocProvider(create: (context) => KisiDetayCubit()),
+        BlocProvider(create: (context) => AnasayfaCubit()),
+      ],
+      child: MaterialApp(
+        title: 'öğretmen ekranı',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const Anasayfa(),
+      ),
+    );
+  }
+}
