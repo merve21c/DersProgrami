0de079779fd892c76891305a9bffd2cb222bc8cc
merve21c <mervecanas.21@gmail.com>
2023-01-10 11:09:01 +0300

    Update DersBilgisi.dart

--- lib/Öğrenci/DersBilgisi.dart
+++ lib/Öğrenci/DersBilgisi.dart
@@ -1,25 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
-
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  static const String _title = 'Flutter Tutorial';
-
-  @override
-  Widget build(BuildContext context) {
-    return   MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: _title,
-      home: DersBilgisi(),
-
-    );
-  }
-}
-
-
 class DersBilgisi  extends StatelessWidget {
   const DersBilgisi({Key? key}) : super(key: key);
 
